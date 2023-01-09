f09bddb528695d2e42bddd5190a98c681c2cc1f5
jiangwei <jiangwei@atguigu.com>
2023-01-09 21:20:34 +0800

    测试SSH

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 7777777
+ * @create 2023-01-09-21:19
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test SSH");
+    }
+}
